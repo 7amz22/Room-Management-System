9e4404d40736e65e4efc455775eae65573e86ef6
Michael John D. Tamayo <michaeltamayo430@gmail.com>
2025-03-26 13:11:31 +0800

    Delete JS/history.js

--- JS/history.js
+++
@@ -1,7 +0,0 @@
-const DropdownBtn = document.getElementById("DropdownBtn");
-const DropdownContent = document.getElementById("DropdownContent");
-
-DropdownBtn.addEventListener("click", (event) => {
-    event.stopPropagation(); 
-    DropdownContent.classList.toggle("show");
-});
