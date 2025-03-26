ac581a07a8aa7e93977ad1be6dc2c55307a58237
Michael John D. Tamayo <michaeltamayo430@gmail.com>
2025-03-26 13:10:26 +0800

    Add files via upload

---
+++ JS/NavigationAdmin.js
@@ -0,0 +1,8 @@
+
+const DropdownBtn = document.getElementById("DropdownBtn");
+const DropdownContent = document.getElementById("DropdownContent");
+
+DropdownBtn.addEventListener("click", (event) => {
+    event.stopPropagation(); // Prevent immediate closing when clicked
+    DropdownContent.classList.toggle("show");
+});
